ef11432d7e21360eb0401771fece3365a9e7905c
professor91 <keshavcode3@gmail.com>
2022-02-02 23:31:43 +0530

    Added Skills.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react"
 import Card from "./components/Card"
 import About from "./components/About";
 import Footer from "./components/Footer";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="min-h-screen py-10 px-3 sm: px-5 bg-gray-100">
       <Card />
       <About />
+      <Skills />
       <Footer />
     </div>  
   );
