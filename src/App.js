70c2d31f5b633b69d5f2af7b961f628cd6af0486
professor91 <keshavcode3@gmail.com>
2022-02-03 11:35:05 +0530

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import NavBar from "./components/NavBar";
 import Card from "./components/Card"
 import About from "./components/About";
 import Footer from "./components/Footer";
@@ -6,7 +7,9 @@ import Skills from "./components/Skills";
 
 function App() {
   return (
-    <div className="min-h-screen py-10 px-3 sm: px-5 bg-gray-100">
+    <div 
+      className="min-h-screen py-5 px-10 sm: px-5 bg-gray-100">
+      <NavBar />
       <Card />
       <About />
       <Skills />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'body' : '#f3f4f6',
+        'selected-text' : '#2196f3',
+      },
+    },
   },
   plugins: [],
 }
