05c188f51a5b0a360d23ac51d37219dd94e1e3bf
professor91 <keshavcode3@gmail.com>
2022-02-17 22:29:35 +0530

    New Version updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from "react"
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// component imports
 import NavBar from "./components/NavBar";
-import Card from "./components/Card"
 import About from "./components/About";
+import Project from "./components/Projects"
 import Footer from "./components/Footer";
-import Skills from "./components/Skills";
+// import Skills from "./components/Skills";
 
 function App() {
   return (
-    <div 
-      className="min-h-screen py-5 px-10 sm: px-5 bg-gray-100">
-      <NavBar />
-      <Card />
-      <About />
-      <Skills />
-      <Footer />
-    </div>  
+      <div 
+        className="max-w-screen-lg mx-auto min-h-screen sm: px-10 md:px-14">
+          {/* py-5 px-10 sm: px-5 bg-gray-100 */}
+
+        <Router>
+          <NavBar />
+          <hr></hr>
+
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/projects" element={<Project />} />
+            {/* <Route path="/skills" element={Skills} /> */}
+          </Routes>
+        
+          <Footer />
+        </Router>
+      </div>  
   );
 }
 
