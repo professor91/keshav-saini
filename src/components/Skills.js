41103fde867df25b19b419f21bfba5819dfd7e3d
professor91 <keshavcode3@gmail.com>
2022-02-19 18:25:39 +0530

    Added Skills.js

---
+++ src/components/Skills.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+function Skills() {
+    return(
+        <div 
+            className="mt-10">
+            <p
+                className="text-3xl font-bold">
+                Skills
+            </p>
+            <p className="text-gray-600">order reflects my proficiency</p>
+            <div
+                className="flex justify-between">
+                <div>
+                    <p
+                        className="text-gray-600 leading-relaxed mt-4">
+                        Languages
+                    </p>
+                    <ul
+                        className="list-disc mt-2 mx-4 text-gray-600 ">
+                        <li>C++</li>
+                        <li>Python</li>
+                        <li>C</li>
+                        <li>JAVA</li>
+                    </ul>
+                </div>
+                <div>
+                    <p
+                        className="text-gray-600 leading-relaxed mt-4">
+                        Frameworks
+                    </p>
+                    <ul
+                        className="list-disc mt-2 mx-4 text-gray-600 ">
+                        <li>ReactJS</li>
+                        <li>Qt</li>
+                    </ul>
+                </div>
+                <div>
+                    <p
+                        className="text-gray-600 leading-relaxed mt-4">
+                        Tools
+                    </p>
+                    <ul
+                        className="list-disc mt-2 mx-4 text-gray-600 ">
+                        <li>Git</li>
+                        <li>Linux</li>
+                        <li>CMake</li>
+                        <li>Google Colab</li>
+                    </ul>
+                </div>
+                <div>
+                    <p
+                        className="text-gray-600 leading-relaxed mt-4">
+                        STEM
+                    </p>
+                    <ul
+                        className="list-disc mt-2 mx-4 text-gray-600 ">
+                        <li>Statistics</li>
+                        <li>Linear Algebra</li>
+                        <li>Public Finance</li>
+                        <li>Operating Systems</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Skills;
+
