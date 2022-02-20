b578ac8b298f33c95caa28b8ddb3a148fdb1e1d8
professor91 <keshavcode3@gmail.com>
2022-02-20 19:55:21 +0530

    Improved design of projects list

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,20 @@
 import React from "react";
 import NoPreviewImg from "../assets/img/No Preview Available.png";
+import SyllabusWebImg from "../assets/img/syllabusWeb.png"
 
 const Card = (props) => {
+    const socials= []
+
+    for (let social of props.socials) {
+        socials.push(
+            <a className="pr-6"
+                href={social[1]}
+                target="_blank">
+                {social[0]}
+            </a>
+        )
+    }
+
     return (
         <div 
             className="pb-10 block md:flex flex-row justify-evenly">
@@ -9,7 +22,7 @@ const Card = (props) => {
             <div
                 className="img w-2/5 sm:justify-center">
                 <img
-                    src={NoPreviewImg}>
+                    src={props.previewImg}>
                 </img>
             </div>
             <div
@@ -24,7 +37,8 @@ const Card = (props) => {
                     </p>
                 <div
                     className="mt-auto">
-                    <a href={props.githubLink}>GitHub</a>
+                    {socials}
+                    {/* <a href={props.githubLink}>GitHub</a> */}
                 </div>
             </div>
         </div>
@@ -46,11 +60,15 @@ function Projects () {
                     projectName= "Syllabus Web"
                     projectDesc= "Syllabus web is India’s largest course-syllabus database which features an interactive website \
                     that presents syllabus of all courses in Delhi University and their syllabus in a well interactive manner."
+                    previewImg= {SyllabusWebImg}
+                    socials= {[["GitHub", "https://github.com/professor91/SyllabusWeb"], ["Preview Site", "https://professor91.github.io/SyllabusWeb_NewDesign_Preview/"]]}
                 />
                 <Card
                     projectName= "WazirX Scraper"
                     projectDesc= "Carnegie Mellon University’s educational graphics software package. \
                     Includes projects in interactive 3D mesh editing, path tracing, dynamic animation, and physics-based simulation."
+                    previewImg= {NoPreviewImg}
+                    socials= {[["GitHub", "https://github.com/professor91/wazirXAPI"]]}
                 />
             </div>
         </div>
