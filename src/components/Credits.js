8550a04a993503010b51b11b8b55fc0a5dfa3a3e
professor91 <keshavcode3@gmail.com>
2022-11-18 10:41:02 +0530

    added credits for images

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -17,6 +17,7 @@ function Credits () {
                         <a 
                             className="underline"
                             target={"_blank"}
+                            rel="noreferrer"
                             href="https://thenumbat.github.io/">
                             Inspiration
                         </a> for website design
@@ -26,6 +27,7 @@ function Credits () {
                         <a 
                             className="underline"
                             target={"_blank"}
+                            rel="noreferrer"
                             href="https://www.youtube.com/watch?v=zdGZ-NdhDuo&list=PLPxc0y7j97c5U1DHWDr4ZDKRgu8wlKdsi&index=22&t=2110s">
                             Inspiration
                         </a> for footer design
@@ -35,9 +37,57 @@ function Credits () {
                         <a 
                             className="underline"
                             target={"_blank"}
+                            rel="noreferrer"
                             href="https://betterprogramming.pub/how-to-host-your-react-app-on-github-pages-for-free-919ad201a4cb">
                             Guide 
                         </a> to deploy react app on GitHub Pages
+                    </li>
+                    <li
+                        className="mb-1">
+                        <a 
+                            href="https://unsplash.com/@alexbemore?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                            YouTube Photo by <span className="underline">Alexander Shatov</span> on 
+                        </a>  
+                        <a 
+                            className="underline"
+                            href="https://unsplash.com/s/photos/youtube?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                            Unsplash
+                        </a>
+                    </li>
+
+                    <li
+                        className="mb-1">
+                        <a 
+                            href="https://unsplash.com/@alexbemore?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                            Discord Photo by <span className="underline">Alexander Shatov</span> on 
+                        </a>
+                        <a 
+                            className="underline"
+                            href="https://unsplash.com/s/photos/discord?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                            Unsplash
+                        </a>
+                    </li>
+                    <li
+                        className="mb-1">
+                        <a 
+                            href="https://unsplash.com/@alexbemore?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                            Spotify Photo by <span className="underline">Alexander Shatov</span> on </a>
+                        <a 
+                            className="underline"
+                            href="https://unsplash.com/s/photos/spotify?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                            Unsplash
+                        </a>
+                    </li>
+                    <li
+                        className="mb-1">
+                        <a 
+                            href="https://unsplash.com/es/@sunder_2k25?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                            WazirX Photo by <span className="underline">Sunder Muthukumaran</span> on </a>
+                        <a 
+                            className="underline"
+                            href="https://unsplash.com/s/photos/wazirX?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                            Unsplash
+                        </a>
                     </li>
                 </ul>
             </div>
